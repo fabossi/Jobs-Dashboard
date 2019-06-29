d3775d2637d94b224cd195cb1692dfcfdc165e1d
Felipe Fabossi <felipe.augusto.fabossi@ibm.com>
2019-06-29 11:57:32 -0300

    [FABOSSI] First real commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeaderSecondaryComponent } from './header-secondary/header-secondary/header-secondary.component';
+import { JobsViewComponent } from './jobs-view/jobs-view.component';
+import { MoreVacanciesComponent } from './more-vacancies/more-vacancies.component';
+import { FooterComponent } from './footer/footer.component';
+import { MoreDetailsComponent } from './more-details/more-details.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeaderSecondaryComponent,
+    JobsViewComponent,
+    MoreVacanciesComponent,
+    FooterComponent,
+    MoreDetailsComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/more-vacancies/more-vacancies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-more-vacancies',
+  templateUrl: './more-vacancies.component.html',
+  styleUrls: ['./more-vacancies.component.scss']
+})
+export class MoreVacanciesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
